75b7189eb744e76dd28817bf5291a87f6577cb69
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2023-06-24 17:58:21 -0300

    Use tslib import helpers

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,11 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var box_1 = __importStar(require("./components/box"));
+var tslib_1 = require("tslib");
+var box_1 = tslib_1.__importStar(require("./components/box"));
 var spacer_1 = require("./components/spacer");
-var data_json_1 = __importDefault(require("./components/data.json"));
-var info_1 = __importDefault(require("./other/info"));
-var box_2 = __importStar(require("./other/box"));
+var data_json_1 = tslib_1.__importDefault(require("./components/data.json"));
+var info_1 = tslib_1.__importDefault(require("./other/info"));
+var box_2 = tslib_1.__importStar(require("./other/box"));
 var wrongWidthType = {
     width: "not-a-number",
 };
